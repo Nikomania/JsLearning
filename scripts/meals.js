6c924621cf19a9d5087b5c7da7f51a0b4afaade8
Thiago <thiagocrrabelo@gmail.com>
2022-05-24 02:33:43 -0300

    Set up for the page that will be used to implement javascript
    index.html modification, creation of CSS repository, as well as of a css file;
    Creation of meals.js, which will be the meal's database.

---
+++ scripts/meals.js
@@ -0,0 +1,7 @@
+const meals = [{
+    nome: 'Sashimi',
+    bg_img: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2014/10/sashimi_salmao_especial.jpg',
+    description: 'Sashimi, specialty of Japanese cuisine, fresh fish served raw.',
+    price: 3.15
+    }
+]
